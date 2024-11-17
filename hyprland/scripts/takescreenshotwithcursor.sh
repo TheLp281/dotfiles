a3a3417535f9896db86f64bf0efac55fd13e37ff
TheLp281 <efekantunc0@gmail.com>
2024-11-17 22:29:12 +0300

    add screenshot with cursor

---
+++ hyprland/scripts/takescreenshotwithcursor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+grim /tmp/fullscreen_screenshot.png
+scrot -p /tmp/cursor_only.png
+convert /tmp/cursor_only.png -fuzz 10% -transparent black /tmp/cursor_only_transparent.png
+convert /tmp/fullscreen_screenshot.png /tmp/cursor_only_transparent.png -composite ~/Pictures/final_screenshot.png
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+convert /tmp/fullscreen_screenshot.png /tmp/cursor_only_transparent.png -composite ~/Pictures/screenshot_$timestamp.png
+rm /tmp/fullscreen_screenshot.png /tmp/cursor_only.png /tmp/cursor_only_transparent.png
+
